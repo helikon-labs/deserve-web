086ccc5b2b029701ba20c1d23abc79ab4f4a30bd
kukabi <kutsalbilgin@gmail.com>
2026-05-31 18:54:05 +0300

    Update locations.

--- src/data/node.ts
+++ src/data/node.ts
@@ -21,20 +21,19 @@ const ASSET_HUB_LOCATIONS: NodeLocation[] = [
     { id: 0, city: 'Atlanta', latitude: 33.749, longitude: -84.388, slug: 'atlanta' },
     { id: 1, city: 'Gravelines', latitude: 50.988, longitude: 2.128, slug: 'gravelines' },
     { id: 2, city: 'İstanbul', latitude: 41.0082, longitude: 28.9784, slug: 'istanbul' },
-    { id: 3, city: 'Johannesburg', latitude: -26.204, longitude: 28.047, slug: 'johannesburg' },
-    { id: 4, city: 'Limburg', latitude: 50.384, longitude: 8.05, slug: 'limburg' },
-    { id: 5, city: 'London', latitude: 51.507, longitude: -0.128, slug: 'london' },
-    { id: 6, city: 'Miami', latitude: 25.762, longitude: -80.192, slug: 'miami' },
-    { id: 7, city: 'Montréal', latitude: 45.502, longitude: -73.567, slug: 'montreal' },
-    { id: 8, city: 'Mumbai', latitude: 19.076, longitude: 72.878, slug: 'mumbai' },
-    { id: 10, city: 'Seattle', latitude: 47.606, longitude: -122.332, slug: 'seattle' },
-    { id: 11, city: 'Singapore', latitude: 1.352, longitude: 103.82, slug: 'singapore' },
-    { id: 12, city: 'Sydney', latitude: -33.869, longitude: 151.209, slug: 'sydney' },
-    { id: 14, city: 'Warsaw', latitude: 52.23, longitude: 21.012, slug: 'warsaw' },
+    { id: 3, city: 'Limburg', latitude: 50.384, longitude: 8.05, slug: 'limburg' },
+    { id: 4, city: 'London', latitude: 51.507, longitude: -0.128, slug: 'london' },
+    { id: 5, city: 'Miami', latitude: 25.762, longitude: -80.192, slug: 'miami' },
+    { id: 6, city: 'Montréal', latitude: 45.502, longitude: -73.567, slug: 'montreal' },
+    { id: 7, city: 'Mumbai', latitude: 19.076, longitude: 72.878, slug: 'mumbai' },
+    { id: 8, city: 'Seattle', latitude: 47.606, longitude: -122.332, slug: 'seattle' },
+    { id: 9, city: 'Singapore', latitude: 1.352, longitude: 103.82, slug: 'singapore' },
+    { id: 10, city: 'Sydney', latitude: -33.869, longitude: 151.209, slug: 'sydney' },
+    { id: 11, city: 'Warsaw', latitude: 52.23, longitude: 21.012, slug: 'warsaw' },
 ];
 
 const CORETIME_LOCATIONS: NodeLocation[] = [
-    { id: 2, city: 'İstanbul', latitude: 41.0082, longitude: 28.9784, slug: 'istanbul' },
+    { id: 0, city: 'İstanbul', latitude: 41.0082, longitude: 28.9784, slug: 'istanbul' },
 ];
 
 const NODE_LOCATIONS: Record<Chain, NodeLocation[]> = {
